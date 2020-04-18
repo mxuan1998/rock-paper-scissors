fbde2a8c0983b63c48313f510564b145350a135e
mxuan1998 <mxuan1990@gmail.com>
2020-04-18 22:45:30 +0400

    style.css

---
+++ script.js
@@ -0,0 +1,50 @@
+let playerScore = 0;
+let computerScore = 0;
+let draw = 0;
+let rounds = 0;
+let result = "";
+
+    function computerPlay(){
+        let action = Math.floor(Math.random()*3);
+        if (action === 0){
+            return "Rock";
+        } else if (action === 1){
+            return "Paper";
+        } else {
+            return "Scissors";
+        }
+    }
+    
+    function playRound(playerSelection, computerSelection){
+        let playerSelection = playerSelection.toLowerCase();
+        gameResult.classList.add("gameResult");
+
+        if (playerSelection === computerSelection) {
+            gameResult.textContent = "It's a tie.";
+            resultsContainer.appendChild(gameResult);
+
+        } else if (playerSelection === "paper" && computerSelection === "rock"
+            || playerSelection === "rock" && computerSelection === "scissors"
+            || playerSelection === "scissors" && computerSelection === "paper"){
+            gameResult.textContent = "Your won!";
+            resultsContainer.appendChild(gameResult);
+            playerWins +=1;
+            document.getElementById("playerScoreContainer").textContent="Your score: " + playerWins;   
+        } else {
+            gameResult.textContent = "Computer won!";
+            resultsContainer.appendChild(gameResult);
+            computerWins +=1;
+            document.getElementById("computerScoreContainer").textContent="Computer score: " + computerWins; 
+        }
+    }
+
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            playerSelection = button.value;
+            computerSelection = computerPlay();
+            playRound();
+            rounds +=1;
+        });
+    });
